4f7346b15b9d098f87930c603b4091cab5e75b1d
Jiahui Huang <jiahuihuang@Jiahuis-MacBook-Pro.local>
2022-06-06 17:33:50 -0400

    bug fix

--- react-app/src/InventoryList.js
+++ react-app/src/InventoryList.js
@@ -66,7 +66,7 @@ class InventoryList extends Component {
                 size="sm"
                 color="danger"
                 tag={Link}
-                onClike={() => this.removeInv(inventory._id)}
+                onClick={() => this.removeInv(inventory._id)}
               >
                 Delete
               </Button>
